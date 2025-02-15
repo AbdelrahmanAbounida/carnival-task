061607f8659015778e615cfd65be71b408bb7658
Abdelrahman Abounida <abdelrahmanabounida@Abdelrahmans-MacBook-Pro.local>
2025-02-15 20:48:01 +0100

    ci: fix publish pipline
    ci: fix publish to ecr
    
    ci: prevent deploy to ecr before build and make build on any push
    
    ci: dummpy ecs deploy pipeline
    
    ci: fix publish ecr
    
    ci: rm build and merge with publish
    
    ci: publish pipeline on any push

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from carnival.core.logger import logger
 
 if __name__ == "__main__":
     # TODO:: Load from .env and scale workers
-    logger.info("\nStarting Carnival compilance Service 🚀 \n")
+    logger.info("\nStarting Carnival compilance Service 🎉 \n")
     uvicorn.run("carnival.app:app", host="0.0.0.0", port=7000, reload=True)
