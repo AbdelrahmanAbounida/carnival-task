758145420689bc1567dbae52ac2e78acd2b6abda
Abdelrahman Abounida <abdelrahmanabounida@Abdelrahmans-MacBook-Pro.local>
2025-02-17 01:00:12 +0100

    feat: load testing

--- tests/load/locustfile.py
+++ tests/load/locustfile.py
@@ -1,8 +1,15 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, constant_pacing
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
-class CompilantServiceUser(HttpUser):
+class ComplianceTestUser(HttpUser):
+    wait_time = constant_pacing(1)  # 1 sec between requests
+
     @task
-    def hello_world(self):
-        self.client.post("/api/v1/compilance")
-        self.client.get("/world")
+    def check_compliance_route(self):
+        params = {"departure_port": "OMSTQ", "arrival_port": "AEJEA"}
+        headers = {"server-api-key": os.environ.get("SERVER_API_KEY")}
+        self.client.get("/api/v1/compilance", params=params, headers=headers)
